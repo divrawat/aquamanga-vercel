6f483f57bfff3c1006aea1c2163226bcb9ae42cb
divrawat <divrawat2001@gmail>
2024-11-13 19:13:28 +0530

    sf0esr5gf

---
+++ config.js
@@ -0,0 +1,43 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+// export const BACKEND_DOMAIN = 'http://localhost:8000';
+
+export const DOMAIN = 'https://aquamanga.vercel.app';
+export const BACKEND_DOMAIN = 'https://backend-mangawebsite.vercel.app'
+
+
+
+export const DOMAIN_NAME = 'aqua-manga.online';
+export const IMAGES_SUBDOMAIN = "https://pics.aqua-manga.online";
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const APP_NAME = 'Aqua Manga';
+export const NavbarName = "Aqua Manga";
+
+// export const APP_LOGO = `${IMAGES_SUBDOMAIN}/public/logo.webp`;
+export const APP_LOGO = `${DOMAIN}/logo.webp`;
+export const NOT_FOUND_IMAGE = `${IMAGES_SUBDOMAIN}/public/404.webp`;
+export const FAVICON = `${IMAGES_SUBDOMAIN}/public/favicon.png`;
+
+export const navLinks = [
+
+    // { text: 'Action', href: `${DOMAIN}/categories/action?page=1` },
+    // { text: 'Adventure', href: `${DOMAIN}/categories/adventure?page=1` },
+    // { text: 'Shoujo', href: `${DOMAIN}/categories/shoujo?page=1` },
+    // { text: 'Romance', href: `${DOMAIN}/categories/romance?page=1` },
+    // { text: 'Thriller', href: `${DOMAIN}/categories/drama?page=1` },
+    // { text: 'Fantasy', href: `${DOMAIN}/categories/fantasy?page=1` },
+    // { text: 'Comedy', href: `${DOMAIN}/categories/comedy?page=1` },
+    // { text: 'New Chapters', href: `${DOMAIN}/new-chapters` },
+    // { text: 'New Mangas', href: `${DOMAIN}/new-mangas` },
+    // { text: 'Contact', href: `${DOMAIN}/contact` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import "@/styles/globals.css";
+import NProgress from 'nprogress';
+import Router from 'next/router';
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+NProgress.configure({ showSpinner: false })
+import { useEffect } from 'react';
+import Popup from "@/components/Popup";
+
+function MyApp({ Component, pageProps }) {
+  /*
+  useEffect(() => {
+    const disableRightClick = (event) => { event.preventDefault(); };
+    document.addEventListener('contextmenu', disableRightClick);
+  }, []);
+  */
+
+  return (
+    <>
+      <Popup />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
